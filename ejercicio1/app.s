2b9411458c92a627fc4aa75a206eb152934485c9
Ulises Valentin Ortiz <ulises.valentin.ortiz@mi.unc.edu.ar>
2025-05-26 21:00:08 -0300

    app.s cleaned and file background created

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -10,53 +10,16 @@
 	.extern draw_circle
 	.extern draw_rectangle
 	.extern coord_to_addr
-
+	.extern background
 main:
-	mov x20, x0         // Guarda la dirección base del framebuffer en x20
-	mov x0, x20         // framebuffer base
-
-	// x0 contiene la direccion base del framebuffer
  	mov x20, x0	// Guarda la dirección base del framebuffer en x20
-	//---------------- CODE HERE ------------------------------------
 
-	movz x10, 0xFFAD, lsl 16
-	movk x10, 0xD8E6, lsl 00
-
-	mov x2, SCREEN_HEIGH         // Y Size
-loop1:
-	mov x1, SCREEN_WIDTH         // X Size
-loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4	   // Siguiente pixel
-	sub x1,x1,1	   // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1	   // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
-
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
-
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
-
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
-
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
-
-	// And bit a bit mantiene el resultado del bit 2 en w10
-	and w11, w10, 0b10
-
-	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
-	// efectivamente, su valor representará si GPIO 2 está activo
-	lsr w11, w11, 1
-
-	//---------------------------------------------------------------
-	// Infinite Loop
+	// PINTAR FONDO 
+	mov x0, x20
+	bl background
 
 	//SOL
-    mov x0, x20       // <- 🔧 restaurar framebuffer base
+    mov x0, x20       // restaurar framebuffer base
     mov x1, #610      // centro x
     mov x2, #15      // centro y
     mov x3, #80       // radio
@@ -64,6 +27,7 @@ loop0:
     movk x4, 0xFF00, lsl 0 // amarillo: 0xFFFFFF00
     bl draw_circle
 
+	// DIBUJO DE SNOOPY
 	mov x0, x20      //
 	bl draw_snoopy   // 
 
