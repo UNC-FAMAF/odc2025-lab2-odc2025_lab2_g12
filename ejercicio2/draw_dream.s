b0bf000e2bd748d21043046c3c66c7289cc36027
cotib2 <cbuteler@mi.unc.edu.ar>
2025-06-01 20:12:59 -0300

    dreaming cloud

--- ejercicio2/draw_dream.s
+++ ejercicio2/draw_dream.s
@@ -4,19 +4,44 @@
 
 .extern draw_circle
 .extern draw_rectangle
-.global draw_dream
 
-draw_dream_1: 
+.global draw_dream_1
+draw_dream_1:
+
 // creo stack frame y guardo registros
     sub sp, sp, #32             
     stp x29, x30, [sp, #0]      // Guardar FP, LR de draw_snoopy
     str x20, [sp, #16]          // Guardar x20 de main (el fb base original)
 
-    mov x1, #230    // xc
-    mov x2, #230    // yc
+    mov x1, #160    // x
+    mov x2, #150    // y
     mov x3, #10     // r
-    movz x4, 0xFFFF, lsl 16 // color
-    movk x4, 0xFFFF, lsl 0  //
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #160    // x
+    mov x2, #170    // y
+    mov x3, #10     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #150    // x
+    mov x2, #160    // y
+    mov x3, #10     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #170    // x
+    mov x2, #160    // y
+    mov x3, #10     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
     bl draw_circle
 
 //FIN
@@ -24,3 +49,99 @@ draw_dream_1:
     ldp x29, x30, [sp, #0]      // Restaurar FP, LR de draw_snoopy
     add sp, sp, #32             // Destruir stack frame
     ret
+
+.global draw_dream_2
+draw_dream_2:
+
+// creo stack frame y guardo registros
+    sub sp, sp, #32             
+    stp x29, x30, [sp, #0]      // Guardar FP, LR de draw_snoopy
+    str x20, [sp, #16]          // Guardar x20 de main (el fb base original)
+
+    // circulos arriba y=150
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #130    // x
+    mov x2, #150    // y
+    mov x3, #13     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #150    // x
+    mov x2, #145    // y
+    mov x3, #13     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #170    // x
+    mov x2, #150    // y
+    mov x3, #13     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    // circulos abajo y=170
+
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #130    // x
+    mov x2, #180    // y
+    mov x3, #13     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #150    // x
+    mov x2, #185    // y
+    mov x3, #13     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #170    // x
+    mov x2, #180    // y
+    mov x3, #13     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    // medio izq
+
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #120    // x
+    mov x2, #165    // y
+    mov x3, #10     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    // medio der
+
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #180    // x
+    mov x2, #165    // y
+    mov x3, #10     // r
+    movz x4, 0xFFFF, lsl 16
+    movk x4, 0xFFFF, lsl 0  // color
+    bl draw_circle
+
+    // relleno
+    ldr x0, [sp, #16]   // restaurar framebuffer base  
+    mov x1, #130    // x
+    mov x2, #150    // y
+    mov x3, #43     // ancho
+    mov x4, #30       // alto
+    movz x5, 0xFFFF, lsl 16
+    movk x5, 0xFFFF, lsl 0 
+    bl draw_rectangle
+
+//FIN
+    ldr x20, [sp, #16]          // Restaurar x20 de main
+    ldp x29, x30, [sp, #0]      // Restaurar FP, LR de draw_snoopy
+    add sp, sp, #32             // Destruir stack frame
+    ret
+
